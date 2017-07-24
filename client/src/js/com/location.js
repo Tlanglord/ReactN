e6b41148cea71416f0e37231f3534b8b34738a03
qiang dong <732427480@qq.com>
2017-07-24 12:41:46 +0800

    add location

---
+++ client/src/js/com/location.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dongqiangqiang on 2017/7/24.
+ */
+
+import React from 'react'
+
+class Location extends React.Component {
+    render() {
+        return <div>location</div>
+    }
+}
+
+export default Location;
