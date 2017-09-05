ad80e3eb96d942d0135b31132913512084498f6a
qiang dong <732427480@qq.com>
2017-09-05 19:36:45 +0800

    login.css

--- client/src/js/com/Login.js
+++ client/src/js/com/Login.js
@@ -3,10 +3,25 @@
  */
 import React from 'react'
 import '../css/login.css'
+import '../css/common.css'
 
 export default class Login extends React.Component {
     render() {
         return <div className="login-container">
-            lllllllll</div>
+            <div className="login">
+                <header className="login-header-wrap">
+                    <a href="/" className="fl login-header"></a>
+                </header>
+                <div className="login-body">
+                    <div className="login-left">
+                        <img src="//s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/waimai.png" width="480"
+                             height="370" alt="美团外卖"></img>
+                    </div>
+                    <div className="fr login-right">
+                    </div>
+                </div>
+                <footer></footer>
+            </div>
+        </div>
     }
 }
